5323541d158e568e7f3bdb42d922e3b6f3f9eab5
JessicaBane <jessicambane@gmail.com>
2023-07-27 22:41:49 +0100

    added css

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { useState } from "react";
 import "./App.css";
 import Weather from "./Weather";
 import axios from "axios";
+import Header from "./Header/Header"
+import Footer from "./Footer/Footer"
 
 function App() {
     const [searchQuery, setSearchQuery] = useState("");
     const [location, setLocation] = useState({});
     const [map, setMap] = useState("");
     const [errorMessage, setErrorMessage] = useState("")
-    const [weather, setWether] = useState([])
+    const [weather, setWether] = useState("")
     const [cityName, setCityName] = useState("")
 
 
@@ -17,33 +19,35 @@ function App() {
     }
 
     async function getCityWeather({lat, lon}){
-        console.log("LAT HERE" + lat)
-        console.log(lon)
+        try{
             // const API = `http://localhost:8082/weather?lat=${lat}&lon=${lon}`
             const API = `https://three01-city-explorer-api.onrender.com/weather?lat=${lat}&lon=${lon}`
-            console.log(API)
+            setErrorMessage("")
             const res = await axios.get(API)
-            console.log(res)
             setWether(res.data)
+        }catch(error){
+            setErrorMessage(`${error}`)
+            setLocation({})
+            setMap("")
+            setWether("")
+        }
     }
 
     async function getLocation(event) {
-        setWether([])
+        setWether("")
+        setCityName(searchQuery)
+
         try {
             event.preventDefault()
             setErrorMessage("")
             const API = `https://eu1.locationiq.com/v1/search?key=${process.env.REACT_APP_API_KEY}&q=${searchQuery}&format=json`;
             const res = await axios.get(API);
             const newData = res.data[0]
-            console.log(newData)
             setLocation(newData);
             handleMap(newData);
-            // console.log(map)
             getCityWeather(newData)
-            setCityName(searchQuery)
+            // setCityName(searchQuery)
         } catch (error) {
-            // error.code = "STATUS"
-            console.log(error);
             setErrorMessage(`${error}`)
             setLocation({})
             setMap("")
@@ -54,27 +58,27 @@ function App() {
     function handleMap(data) {
         const API = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_KEY}&center=${data.lat},${data.lon}&zoom=10`;
         setMap(API);
-        console.log("Setting map")
-        console.log(map)
     }
 
     return (
         <>
+        <Header/>
         <div className="App">
-            <form onSubmit={getLocation}>
-                <input type="text" placeholder="search for a city" name="input" onChange={handleSearch} />
-                <button type="submit">Explore!</button>
+            <form className="form" onSubmit={getLocation}>
+                <input className="formInput" type="text" placeholder="search for a city" name="input" onChange={handleSearch} />
+                <button className="button" type="submit">Explore!</button>
             </form>
         </div>
         <div className="searchInfo">
             {/* <p className="errorMessage">{errorMessage}</p> */}
-            {errorMessage && <p>{errorMessage}</p>}
-            {location && <p>{location.display_name}</p>}
+            {errorMessage && <p className="errorMessage">{errorMessage}</p>}
+            {location && <p id="locationName">{location.display_name}</p>}
             <div className="returnedData">
             {map && <img className="mapImg" src={map} alt="map" />}
-            {weather.length > 0 && <Weather weather={weather} city={cityName}/>}  
+            {weather.length && <Weather className="weather" weather={weather} city={cityName}/>}  
             </div>
         </div>
+        <Footer/>
         </>
     );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -5,36 +5,46 @@ import './Weather.css'
 import React from 'react'
 
 export default function Weather({weather, city}) {
+
     const cityName = city.charAt(0).toUpperCase() + city.slice(1)
 
+
     try{
-        const forcastWeather = weather.map((day, key)=>{
-            const description = day.weather.description
-            console.log(day.weather.description)
+        if(weather !== "Sorry no forecast weather found"){
+            const forcastWeather = weather.map((day, key)=>{
+                const description = day.weather.description
+
+
+                return (
+                    <div className='weatherData' key={day.datetime}>
+                        <h3 className='date'> {day.datetime} </h3>
+                        <p className='weatherDescription'>{description}</p>
+                    </div>
+                )
+            })
 
             return (
-                <div className='weatherData' key={day.datetime}>
-                    <h3 className='date'> {day.datetime} </h3>
-                    <p className='weatherDescription'>{description}</p>
+                <div className="weather-div">
+                    <h2>Weather Forecast for {cityName}</h2>
+                    <div className='weatherContainer'>
+                    {forcastWeather}
+                    </div>
                 </div>
             )
-        })
-
-        return (
-            <div className="weather-div">
-                <h2>Weather Forecast for {cityName}</h2>
-                <div className='weatherContainer'>
-                {forcastWeather}
+        } else {
+            return(
+                <div className="weather-div">
+                    <h2>{cityName}</h2>
+                    <p> Sorry there is no weather forecast for this location</p>
                 </div>
-            </div>
-        )
+            )
+        }
     }catch(error){
-        console.log(error)
         return(
             <div>
-                <h3>{cityName}</h3>
-                <p> Sorry there is no weather forecast for this location</p>
+                <p className='errorMessage'>{error}</p>
             </div>
         )
+
     }
 }
